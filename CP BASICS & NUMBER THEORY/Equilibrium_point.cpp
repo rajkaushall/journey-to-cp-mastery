a2356f2242d7b508ebcf3040411d28ad8cb456bb
Kaushal Raj <RajKaushal005@outlook.com>
2025-04-18 17:30:13 +0530

    Equilibrium_point.cpp

--- CP BASICS & NUMBER THEORY/Equilibrium_point.cpp
+++ CP BASICS & NUMBER THEORY/Equilibrium_point.cpp
@@ -1,32 +1,35 @@
-// Finding Equilibrium index in an array
+Finding Equilibrium index in an array
 
-// Problem Statement: Finding Equilibrium index in an array
+Problem Statement: Finding Equilibrium index in an array
 
-// Given a 0-indexed integer array nums, find the leftmost equilibrium Index.
+Given a 0-indexed integer array nums, find the leftmost equilibrium Index.
 
-// An equilibrium Index is an index at which sum of elements on its left is equal to the sum of element on its right. That is, nums[0] + nums[1] + ... + nums[equilibriumIndex-1] == nums[equilibriumIndex+1] + nums[equilibriumIndex+2] + ... + nums[nums.length-1]. If equilibriumIndex == 0, the left side sum is considered to be 0. Similarly, if equilibriumIndex == nums.length - 1, the right side sum is considered to be 0.
+An equilibrium Index is an index at which sum of elements on its left is equal to the sum of element on its right. That is, nums[0] + nums[1] + ... + nums[equilibriumIndex-1] == nums[equilibriumIndex+1] + nums[equilibriumIndex+2] + ... + nums[nums.length-1]. If equilibriumIndex == 0, the left side sum is considered to be 0. Similarly, if equilibriumIndex == nums.length - 1, the right side sum is considered to be 0.
 
-// Return the leftmost equilibrium Index that satisfies the condition, or -1 if there is no such index.
+Return the leftmost equilibrium Index that satisfies the condition, or -1 if there is no such index.
 
-// Examples:
+Examples:
 
-// Example 1:
-// Input: nums = [2,3,-1,8,4]
-// Output: 3
-// Explanation: The sum of the numbers before index 3 is: 2 + 3 + -1 = 4
-// The sum of the numbers after index 3 is: 4 = 4
+Example 1:
+Input: nums = [2,3,-1,8,4]
+Output: 3
+Explanation: The sum of the numbers before index 3 is: 2 + 3 + -1 = 4
+The sum of the numbers after index 3 is: 4 = 4
 
-// Example 2:
-// Input: nums = [1,-1,4]
-// Output: 2
-// Explanation: The sum of the numbers before index 2 is: 1 + -1 = 0
-// The sum of the numbers after index 2 is: 0
+Example 2:
+Input: nums = [1,-1,4]
+Output: 2
+Explanation: The sum of the numbers before index 2 is: 1 + -1 = 0
+The sum of the numbers after index 2 is: 0
 
 
+    
 
-// ===============================================
-// 🔹 Brute Force Approach 
-// ===============================================
+                                           ===============================================
+                                                    🔹 Brute Force Approach 
+                                           ===============================================
+
+
 
 #include <iostream>
 using namespace std;
@@ -70,9 +73,11 @@ int main() {
 }
 
 
-// ===============================================
-// 🔹 Optimal Approach - Prefix Sum Technique
-// ===============================================
+                                                         ===============================================
+                                                            🔹 Optimal Approach - Prefix Sum Technique
+                                                         ===============================================
+
+                                                             
 
 
 #include <iostream>
